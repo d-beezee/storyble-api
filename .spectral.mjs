cdc6111fd70f84f7c6d61a25e69133753d46555e
davide-bizzi <davide.bizzi@app-quality.com>
2023-01-27 19:43:32 +0100

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MDc1MQ/spectral.js?branch_slug=main&token=566807ce-fb13-4b3d-a43c-59ca3bdcdc94";
+export default { extends: ruleset };
